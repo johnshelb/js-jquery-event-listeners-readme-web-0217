55a4c7a1cb926c26ab4b17458f392504daf3ba7e
John Shelby <johnshelby@yahoo.com>
2017-03-19 18:16:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey")
+  })
+}
+
+function frameIt() {
+  $('img').on('load',function(){
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(k){
+    if (k.which === 71){
+      alert("you typed g")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 
 // call functions here
-
+$(document).ready(function(){
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
